c831387d8368db8fcbaa54f38f3d1476a1289c06
Sai Kumar Peddireddy <sai.peddireddy77@gmail.com>
2020-05-01 20:01:38 +0530

    prefix evaluation is implemented

--- Stack/BasicPostfixEvaluation.cpp
+++ Stack/BasicPostfixEvaluation.cpp
@@ -5,16 +5,22 @@
 using namespace std;
 
 int  EvaluatePostfix(const string expression);
+int  EvaluatePrefix(const string expression);
 bool isOperand(const char& input_char);
 
 int main()
 {
     //infix 2*5+5*5
     string postfix_string = "25*55*+";
+    string prefix_string = "+*25*55";
 
     cout<<"postfix: "<<postfix_string <<'\n';
 
-    cout<<"resilt is: " <<EvaluatePostfix(postfix_string)<<'\n';
+    cout<<"result is: " <<EvaluatePostfix(postfix_string)<<'\n';
+
+    cout<<"prefix: "<<prefix_string <<'\n';
+
+    cout<<"result is: " <<EvaluatePrefix(prefix_string)<<'\n';
 
 }
 
@@ -57,5 +63,34 @@ bool isOperand(const char& input_char)
     const int val = static_cast<int>(input_char) - 48;
 
     return val > 0;
+}
+
+int EvaluatePrefix(const string expression)
+{
+    stack<int> evalution_stack;
+
+    for(auto i = expression.rbegin(); i != expression.rend(); ++i)
+    {
+        if(isOperand(*i))
+        {
+            const auto int_val = static_cast<int>(*i) - 48;
+            evalution_stack.push(int_val);
+        }
+        else
+        {
+            const auto operand1 = evalution_stack.top();
+            evalution_stack.pop();
+
+            const auto operand2 = evalution_stack.top();
+            evalution_stack.pop();
+
+            if(*i == '*')
+                evalution_stack.push(operand1 * operand2);
+            else if(*i == '+')
+                evalution_stack.push(operand1 + operand2);
+        }
+    }
+
+    return evalution_stack.top();
 }
 
