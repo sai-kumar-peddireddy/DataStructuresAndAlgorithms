746d3e11adc8868ca1dbc4f8222ea2d4f1f7353e
Sai Kumar Peddireddy <sai.peddireddy77@gmail.com>
2020-05-04 05:54:17 +0530

    implemented Queue using circular array

---
+++ Queue/QueueOperations.h
@@ -0,0 +1,14 @@
+#ifndef QUEUEOPERATIONS_H
+#define QUEUEOPERATIONS_H
+
+void Enqueue(const size_t& data);
+void Dequeue();
+
+int front();
+
+
+bool isEmpty();
+bool isFull();
+
+
+#endif // QUEUEOPERATIONS_H
